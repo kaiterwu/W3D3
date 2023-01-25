f24d78e3417b008bae53ec7a37a81df48c63a842
Kaiter Wu <wukaiter@gmail.com>
2023-01-25 17:33:13 -0500

    trying to troubleshoot code

--- binary_search.rb
+++ binary_search.rb
@@ -1,13 +1,21 @@
 def binary_search(arr,value)
-    length = arr.length 
-    var = value <=> arr[length/2]
-        if var == -1 
-            binary_search(arr[0...(length/2)],value) 
-        elsif var == 1 
-            binary_search(arr[((length/2)+1)..-1],value)
-        elsif var == 0 
-            return arr.index(value) 
+    length = arr.length
+    if length == 1 
+        if arr[0] == value 
+            return 0 
         else  
             return nil 
+        end 
+    end 
+
+    var = value <=> arr[length/2]
+        if var == -1 
+             binary_search(arr[0...(length/2)],value)
+
+        elsif var == 1 
+             binary_search(arr[((length/2)+1)..-1],value)
+        elsif var == 0 
+            return array.index
         end
 end 
+
