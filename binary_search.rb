99d0f6b87c7972c235dc7b12e8e5a0df7d6e4722
Kaiter Wu <wukaiter@gmail.com>
2023-01-25 16:21:08 -0500

    working on binary search

--- binary_search.rb
+++ binary_search.rb
@@ -1,21 +1,25 @@
 def binary_search(arr,value)
     length = arr.length 
     if length == 1 
-        return value-1 if arr[0] == value 
+        return value-1 if arr[0] == value
+        end
     else  
         return nil 
     end 
 
-    if length.even? 
-        var = value <=> ((arr[length/2] + arr[(length/2)+1])/2)
+    middle_ele = ((arr[length/2] + arr[(length/2)-1])/2)
+    lower_search = binary_search(arr[0..length/2],value) if var == -1 
+    upper_search = binary_search(arr[((length/2)+1)..-1],value) if var == 1 
 
-        binary_search(arr[0..length/2],value) if var == -1 
-        binary_search(arr[((length/2)+1)..-1],value) if var == 1 
+    if length.even? 
+        var = value <=> middle_ele
+        lower_search
+        upper_search
     else  
-        var = value <=> (arr[length/2] + arr[(length/2)+1])/2
-        binary_search(arr[0..length/2],value) if var == -1 
+        var = value <=> arr[length/2]
+        lower_search
         return array.index(value) if var == 0 
-        binary_search(arr[((length/2)+1)..-1],value) if var == 1 
+        upper_search
     end
 
 
