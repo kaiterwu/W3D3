87927c67bfd96fbc17cd799401d30fe102c73374
Kaiter Wu <wukaiter@gmail.com>
2023-01-25 21:00:31 -0500

    editing binary_search code

--- binary_search.rb
+++ binary_search.rb
@@ -1,21 +1,19 @@
 def binary_search(arr,value)
-    length = arr.length
-    if length == 1 
-        if arr[0] == value 
-            return 0 
-        else  
-            return nil 
-        end 
-    end 
-
-    var = value <=> arr[length/2]
+    return nil if arr.empty? 
+    
+    mid = [arr.length/2]
+    var = value <=> arr[mid]
         if var == -1 
-             binary_search(arr[0...(length/2)],value)
+             binary_search(arr[0...mid],value)
 
         elsif var == 1 
-             binary_search(arr[((length/2)+1)..-1],value)
+            result = binary_search(arr[mid+1..-1],value)
+            return nil if result.nil? 
+            return result + mid+1 
+
+             
         elsif var == 0 
-            return array.index
+            return mid 
         end
 end 
 
