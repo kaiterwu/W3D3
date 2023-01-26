ce45ea8a0b990e5b8f5854e0bc279b7e48b1342f
Kaiter Wu <wukaiter@gmail.com>
2023-01-25 21:04:26 -0500

    binary_search code completed,code is functional

--- binary_search.rb
+++ binary_search.rb
@@ -1,7 +1,7 @@
 def binary_search(arr,value)
     return nil if arr.empty? 
     
-    mid = [arr.length/2]
+    mid = arr.length/2
     var = value <=> arr[mid]
         if var == -1 
              binary_search(arr[0...mid],value)
